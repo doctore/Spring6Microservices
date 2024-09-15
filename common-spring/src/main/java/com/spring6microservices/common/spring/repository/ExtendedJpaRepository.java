ee55846704cab70633ff5b92d70ec8d81f3b86e7
doctore <c.javier.reyes.e@gmail.com>
2024-09-15 11:37:35 +0200

    Added several repository base classes and their unit tests

---
+++ common-spring/src/main/java/com/spring6microservices/common/spring/repository/ExtendedJpaRepository.java
@@ -0,0 +1,111 @@
+package com.spring6microservices.common.spring.repository;
+
+import com.spring6microservices.common.core.collection.tuple.Tuple2;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.Expression;
+import jakarta.persistence.criteria.Order;
+import jakarta.persistence.criteria.Predicate;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Extended {@link JpaRepository} to include custom methods we want to share among all repositories.
+ *
+ * @param <T>
+ *    Entity owner of the current repository
+ * @param <ID>
+ *    Primary key of the Entity
+ */
+@NoRepositoryBean
+public interface ExtendedJpaRepository<T, ID extends Serializable> extends JpaRepository<T, ID> {
+
+    /**
+     * Return the internal {@link EntityManager} to provide more functionality to the repositories.
+     *
+     * @return {@link EntityManager}
+     */
+    EntityManager getEntityManager();
+
+
+    /**
+     * Returns the HQL representation of the internal query of the given {@link Query}.
+     *
+     * @param query
+     *    {@link Query} to get its SQL query
+     *
+     * @return {@link String} with the HQL representation of the internal query,
+     *         empty {@link String} is there is any error getting it.
+     */
+    String getHQLQuery(final Query query);
+
+
+    /**
+     * Returns the HQL representation of the internal query of the given {@link TypedQuery}.
+     *
+     * @param query
+     *    {@link TypedQuery} to get its SQL query
+     *
+     * @return {@link String} with the HQL representation of the internal query,
+     *         empty {@link String} is there is any error getting it.
+     */
+    String getHQLQuery(final TypedQuery<?> query);
+
+
+    /**
+     *    Returns SQL {@code equals} operator based on provided {@code sourcePath} and {@code value}, only if {@code value}
+     * is not empty.
+     *
+     * @param criteriaBuilder
+     *    {@link CriteriaBuilder} used to build the {@code equals} clause
+     * @param sourcePath
+     *    {@link Expression} with the property to compare with given {@code value}
+     * @param value
+     *    {@link String} with the value to search in the returned SQL {@code equals} operator
+     *
+     * @return {@link Optional} with SQL {@code equals} operator if {@code value} is not empty,
+     *         {@link Optional#empty()} otherwise
+     */
+    Optional<Predicate> buildEqualsIfNotEmpty(final CriteriaBuilder criteriaBuilder,
+                                              final Expression<String> sourcePath,
+                                              final String value);
+
+
+    /**
+     *    Returns SQL {@code in} operator based on provided {@code sourcePath} and {@code values}, only if {@code values}
+     * is not empty.
+     *
+     * @param sourcePath
+     *    {@link Expression} with the property to compare with given {@code value}
+     * @param values
+     *    {@link Collection} with the values to search in the returned SQL {@code in} operator
+     *
+     * @return {@link Optional} with SQL {@code in} operator if {@code values} is not empty,
+     *         {@link Optional#empty()} otherwise
+     */
+    <E> Optional<Predicate> buildInIfNotEmpty(final Expression<E> sourcePath,
+                                              final Collection<E> values);
+
+
+    /**
+     * Returns a {@link List} with the {@link Order} used in the {@code order by} clause of the query.
+     *
+     * @param criteriaBuilder
+     *    {@link CriteriaBuilder} used to build the {@code order by} clause
+     * @param sourcePathAndIsAsc
+     *    {@link Tuple2} with: {@link Expression} with the property to use in the {@code order by} as {@code left},
+     *    {@code true} if the ordination should be asc or {@code false} otherwise, as {@code right}
+     *
+     * @return {@link List} of {@link Order}
+     */
+    List<Order> buildOrdersBy(final CriteriaBuilder criteriaBuilder,
+                              final Tuple2<Expression<String>, Boolean>... sourcePathAndIsAsc);
+
+}
---
+++ common-spring/src/main/java/com/spring6microservices/common/spring/repository/ExtendedJpaRepositoryImpl.java
@@ -0,0 +1,137 @@
+package com.spring6microservices.common.spring.repository;
+
+import com.spring6microservices.common.core.collection.tuple.Tuple2;
+import com.spring6microservices.common.core.util.CollectionUtil;
+import com.spring6microservices.common.core.util.StringUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.Expression;
+import jakarta.persistence.criteria.Order;
+import jakarta.persistence.criteria.Predicate;
+import lombok.extern.log4j.Log4j2;
+import org.hibernate.query.sqm.internal.QuerySqmImpl;
+import org.hibernate.query.sqm.tree.SqmVisitableNode;
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+import static com.spring6microservices.common.core.util.ExceptionUtil.getFormattedRootError;
+import static java.lang.String.format;
+import static java.util.Optional.ofNullable;
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Extended {@link SimpleJpaRepository} to include custom methods we want to share among all repositories.
+ *
+ * @param <T>
+ *    Entity owner of the current repository
+ * @param <ID>
+ *    Primary key of the Entity
+ */
+@Log4j2
+public class ExtendedJpaRepositoryImpl<T, ID extends Serializable> extends SimpleJpaRepository<T, ID> implements ExtendedJpaRepository<T, ID> {
+
+    private final String QUERY_CLASS_PROXY = "Proxy";
+    private final String QUERY_METHOD_SQL_CONVERSION = "getSqmStatement";
+
+    protected EntityManager entityManager;
+
+    public ExtendedJpaRepositoryImpl(final JpaEntityInformation<T, ?> entityInformation,
+                                     final EntityManager entityManager) {
+        super(
+                entityInformation,
+                entityManager
+        );
+        this.entityManager = entityManager;
+    }
+
+
+    @Override
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+
+    @Override
+    public String getHQLQuery(final Query query) {
+        return ofNullable(query)
+                .map(q -> {
+                    try {
+                        return q.getClass().getName().contains(QUERY_CLASS_PROXY)
+                                ? ((SqmVisitableNode) q.getClass().getMethod(QUERY_METHOD_SQL_CONVERSION).invoke(q)).toHqlString()
+                                : q.unwrap(QuerySqmImpl.class).getSqmStatement().toHqlString();
+
+                    } catch (Exception e) {
+                        log.debug(
+                                format("The was an error getting the HQL equivalent. %s",
+                                        getFormattedRootError(e)
+                                ),
+                                e
+                        );
+                        return null;
+                    }
+                })
+                .orElse("");
+    }
+
+
+    @Override
+    public String getHQLQuery(final TypedQuery<?> query) {
+        return getHQLQuery((Query) query);
+    }
+
+
+    @Override
+    public Optional<Predicate> buildEqualsIfNotEmpty(final CriteriaBuilder criteriaBuilder,
+                                                     final Expression<String> sourcePath,
+                                                     final String value) {
+        return ofNullable(value)
+                .filter(s -> !StringUtil.isBlank(s))
+                .map(v ->
+                        criteriaBuilder.equal(
+                                sourcePath,
+                                v
+                        )
+                );
+    }
+
+
+    @Override
+    public <E> Optional<Predicate> buildInIfNotEmpty(final Expression<E> sourcePath,
+                                                     final Collection<E> values) {
+        return ofNullable(values)
+                .filter(v -> !CollectionUtil.isEmpty(v))
+                .map(v ->
+                        sourcePath.in(
+                                values
+                        )
+                );
+    }
+
+
+    @Override
+    @SafeVarargs
+    public final List<Order> buildOrdersBy(final CriteriaBuilder criteriaBuilder,
+                                           final Tuple2<Expression<String>, Boolean>... sourcePathAndIsAsc) {
+        return ofNullable(sourcePathAndIsAsc)
+                .map(tuples ->
+                        Arrays.stream(tuples)
+                                .map(tuple ->
+                                        tuple._2
+                                                ? criteriaBuilder.asc(tuple._1)
+                                                : criteriaBuilder.desc(tuple._1)
+                                )
+                                .collect(toList())
+                )
+                .orElseGet(ArrayList::new);
+    }
+
+}
